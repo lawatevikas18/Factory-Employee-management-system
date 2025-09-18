c42b9b18dd3b0a5221c2a11df0e989f16cd8ccb2
hajaredatta <hajaredatta320@gmail.com>
2025-09-18 22:51:04 +0530

    space remove

--- UI/src/app/layout/layout-routing.module.ts
+++ UI/src/app/layout/layout-routing.module.ts
@@ -8,7 +8,6 @@ const routes: Routes = [
     path: '',
     component: LayoutComponent,
     children: [
-      { path: 'dashboard', loadChildren: () => import('../component/dashboard/dashboard.module').then(m => m.DashboardModule) },
      { path: 'dashboard', loadChildren: () => import('../component/dashboard/dashboard.module').then(m => m.DashboardModule) },
      { path: 'employee-details', loadChildren: () => import('../component/employee-details/employee-details.module').then(m => m.EmployeeDetailsModule) },
      { path: 'employee-attendance', loadChildren: () => import('../component/attendance/attendance.module').then(m=> m.AttendanceModule)},
